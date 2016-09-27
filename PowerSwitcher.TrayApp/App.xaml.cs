425f17216bca42aba6d7ad3fb79ab45d492baa4e
Petr Houška <houskape@gmail.com>
2016-09-28 00:07:29 +0200

    Added mutex for ensuring only one instance

--- PowerSwitcher.TrayApp/App.xaml.cs
+++ PowerSwitcher.TrayApp/App.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,46 @@ namespace PowerSwitcher.TrayApp
     /// </summary>
     public partial class App : Application
     {
+
+
+        private Mutex _mMutex;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var mutexName = string.Format(CultureInfo.InvariantCulture, "Local\\{{{0}}}{{{1}}}", assembly.GetType().GUID, assembly.GetName().Name);
+
+            bool mutexCreated;
+
+            _mMutex = new Mutex(true, mutexName, out mutexCreated);
+
+            if (!mutexCreated)
+            {
+                _mMutex = null;
+                Current.Shutdown();
+                return;
+            }
+
+            new MainWindow();
+        }
+
+
+        private void DisposeMutex()
+        {
+            if (_mMutex == null) return;
+            _mMutex.ReleaseMutex();
+            _mMutex.Close();
+            _mMutex = null;
+        }
+
+        private void App_OnExit(object sender, ExitEventArgs e)
+        {
+            DisposeMutex();
+        }
+
+        ~App()
+        {
+            DisposeMutex();
+        }
+
     }
 }
