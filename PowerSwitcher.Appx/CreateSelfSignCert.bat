8c1b180730cdd74cb0b2bd21901524bfa57f1b1b
Petr Houška <houskape@gmail.com>
2016-09-30 01:33:00 +0200

    Changed some small issues with AppX generation scripts.

--- PowerSwitcher.Appx/CreateSelfSignCert.bat
+++ PowerSwitcher.Appx/CreateSelfSignCert.bat
@@ -1,5 +1,5 @@
-if not exist "../Build" mkdir ../Build
-if not exist "../Build/Certs" mkdir ../Build/Certs
+if not exist "..\Build" mkdir ..\Build
+if not exist "..\Build\Appx" mkdir ..\Build\Certs
 
 MakeCert.exe -r -h 0 -n "CN=Petrroll, L=Prague, C=Czech Republic" -eku 1.3.6.1.5.5.7.3.3 -pe -sv ../Build/Certs/my.pvk ../Build/Certs/my.cer
 pvk2pfx.exe -pvk ../Build/Certs/my.pvk -spc ../Build/Certs/my.cer -pfx ../Build/Certs/my.pfx
