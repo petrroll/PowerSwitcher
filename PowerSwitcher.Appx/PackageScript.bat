e546671fe5995920c4c7cb55e7a287a193b8ee6b
Petr Houška <houskape@gmail.com>
2017-02-04 00:33:39 +0100

    Small tweaks to configs & certs because of MS Store.

--- PowerSwitcher.Appx/PackageScript.bat
+++ PowerSwitcher.Appx/PackageScript.bat
@@ -2,4 +2,5 @@ if not exist "..\Build" mkdir ..\Build
 if not exist "..\Build\Appx" mkdir ..\Build\Appx
 
 MakeAppx pack /o /f MappingFile.txt /p ..\Build\Appx\PowerSwitcher.appx
+copy /y ..\Build\Appx\PowerSwitcher.appx ..\Build\Appx\PowerSwitcher_ns.appx
 signtool.exe sign -f ..\Build\Certs\my.pfx -fd SHA256 -v ..\Build\Appx\PowerSwitcher.appx
