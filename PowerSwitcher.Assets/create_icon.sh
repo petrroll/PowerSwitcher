287790ac821bfb124522fed4d8ce0f3aa0736011
Petr Houška <houskape@gmail.com>
2017-02-28 02:41:48 +0100

    Improved icon & other graphics.

--- PowerSwitcher.Assets/create_icon.sh
+++ PowerSwitcher.Assets/create_icon.sh
@@ -1,10 +1,19 @@
 #16x16 has to be done manually
-sizes="20 32 44 50 64 128 150 256 512"
+sizesNoAA="20 32"
+sizesAA="40 44 46 50 64 128 150 256 512"
 names=""
-for i in $sizes 
+for i in $sizesNoAA 
 do 
     convert +antialias -background none plug.svg -filter Lanczos -resize ${i}x${i} png8:icon_${i}x${i}.png
     names=$names" icon_"${i}x${i}".png" 
 done
+
+for i in $sizesAA
+do 
+    convert +antialias -background none plug.svg -resize ${i}x${i} icon_${i}x${i}.png
+    names=$names" icon_"${i}x${i}".png" 
+done
+
+names=$names" icon_16x16.png"
 
 convert $names Tray.ico
