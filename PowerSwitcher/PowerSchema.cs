7791a52c5859a1e177c4f589b75d262283193c07
Petr Houška <houskape@gmail.com>
2017-03-08 19:55:43 +0100

    Better PowerSchemas debug.

--- PowerSwitcher/PowerSchema.cs
+++ PowerSwitcher/PowerSchema.cs
@@ -31,5 +31,10 @@ namespace PowerSwitcher
             this.Guid = guid;
             this.IsActive = isActive;
         }
+
+        public override string ToString()
+        {
+            return $"{Name}:{IsActive}";
+        }
     }
 }
